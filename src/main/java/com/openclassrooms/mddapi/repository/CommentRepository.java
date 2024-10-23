1548111cd74c171e8c42692b062eefcd3945d2f3
ZaeRon007 <julien.pelissier8@orange.fr>
2024-10-23 18:41:17 +0200

    creation of repository for article and comment

--- src/main/java/com/openclassrooms/mddapi/repository/CommentRepository.java
+++ src/main/java/com/openclassrooms/mddapi/repository/CommentRepository.java
@@ -2,9 +2,9 @@ package com.openclassrooms.mddapi.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import com.openclassrooms.mddapi.model.ArticleEntity;
+import com.openclassrooms.mddapi.model.CommentEntity;
 
 @Repository
-public interface ArticleRepository extends CrudRepository<ArticleEntity, Integer>{
+public interface CommentRepository extends CrudRepository<CommentEntity, Integer>{
     
 }
