8595d583acb5fde92d7be411c1c1299bcff2b97b
ZaeRon007 <julien.pelissier8@orange.fr>
2024-11-13 15:51:25 +0100

    articleEntity update

--- front/src/app/core/interface/article.interface.ts
+++ front/src/app/core/interface/article.interface.ts
@@ -1,7 +1,7 @@
 import { themeEntity } from "./themeEntity";
 
-export interface ArticleEntity {
-    themeId: number;
-    titre: string;
-    content: string;
+export class ArticleEntity {
+    themeId!: number;
+    titre!: string;
+    content!: string;
 }
