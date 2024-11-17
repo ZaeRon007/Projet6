0f1e2e45dfee774251beb2624f92c3328ec687bd
ZaeRon007 <julien.pelissier8@orange.fr>
2024-11-17 12:44:54 +0100

    work in progress: update for articles component

--- front/src/app/mddapi/components/articles/articles.component.ts
+++ front/src/app/mddapi/components/articles/articles.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ArticleService } from '../../services/articlesService';
+import { getArticle } from 'src/app/core/models/dto/getArticle';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../../services/userService';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
+  articles: getArticle[] = [];
+  private sub!: Subscription;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private articleService: ArticleService,
+              private userService: UserService,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.sub = this.articleService.fetch().subscribe((articles: getArticle[]) => {
+      this.articles = articles;
+
+      console.log(articles);
+    })
+  }
+
+  getUserById(id: number){
+    return this.userService.getUserById(id).subscribe();
   }
 
   create(): void {
     this.router.navigateByUrl('articles/create');
   }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+  
 }
