ea50d6ec3f6828c9cbb4ab3346067a652389164e
ZaeRon007 <julien.pelissier8@orange.fr>
2024-11-17 12:10:03 +0100

    update for create component

--- front/src/app/mddapi/components/create/create.component.ts
+++ front/src/app/mddapi/components/create/create.component.ts
@@ -2,9 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Article } from 'src/app/core/interface/article.interface';
-import { theme } from 'src/app/core/interface/theme.interface';
+import { themeEntity } from 'src/app/core/models/themeEntity';
 import { environment } from 'src/environments/environment.prod';
+import { ThemeService } from '../../services/themeService';
+import { CreateArticle } from 'src/app/core/models/dto/createArticle';
 
 
 @Component({
@@ -15,39 +16,22 @@ import { environment } from 'src/environments/environment.prod';
 export class CreateComponent implements OnInit {
   private apiUrl = environment.baseUrl;
 
-  monFormulaire: FormGroup;
-  title!: string;
-  content!: string;
-  options: theme[] | undefined;
+  options: themeEntity[] = [];
+  article: CreateArticle = new CreateArticle;
 
   constructor(private router: Router,
               private httpclient: HttpClient,
-              private formBuilder: FormBuilder) 
-    {    
-      this.monFormulaire = this.formBuilder.group({
-        options: ['', Validators.required],
-        title: ['', Validators.required],
-        content: ['', Validators.required],
-    })
+              private themeService: ThemeService) {    
    }
 
   ngOnInit(): void {
-    this.title = "";
-    this.content = "";
-  }
-
-  onClick(): void {
-    this.httpclient.get<theme[]>(`${this.apiUrl}themes`).subscribe((response: theme[]) => {
-      this.options = response;
-  });
+    this.themeService.fetch().subscribe((themes: themeEntity[]) => {
+      this.options = themes;
+    })
   }
 
   onSubmit(){
-    if(this.monFormulaire.valid){
-      let article = this.monFormulaire.value as Article;
-      this.httpclient.post<Article>(`${this.apiUrl}articles`, article).subscribe();
-    }
-
+    this.httpclient.post<CreateArticle>(`${this.apiUrl}article`, this.article).subscribe();
     this.router.navigateByUrl('/articles/home');
   }
 
