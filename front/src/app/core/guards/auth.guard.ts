3937a4b6ee878540f5f5718b0415e1b3fd1b2401
ZaeRon007 <julien.pelissier8@orange.fr>
2024-11-19 14:53:14 +0100

    fix for unguard

--- front/src/app/core/guards/auth.guard.ts
+++ front/src/app/core/guards/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
         if(this.authService.isLoggedIn())
             return true;
         else {
-            this.router.navigateByUrl('/auth');
+            this.authService.logOut();
             return false;
         }
     }
