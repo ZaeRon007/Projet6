45b38a27d75a10508e1a195f6fb65ea91f6304fd
ZaeRon007 <julien.pelissier8@orange.fr>
2024-11-20 09:02:09 +0100

    update for theme component

---
+++ front/src/app/core/models/subscribeEntity.ts
@@ -0,0 +1,5 @@
+export class SubscribeEntity {
+    id!: number;
+    userId!: number;
+    themeId!: number;
+}
--- front/src/app/mddapi/components/themes/themes.component.ts
+++ front/src/app/mddapi/components/themes/themes.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { themeEntity } from 'src/app/core/models/themeEntity';
 import { ThemeService } from '../../services/themeService';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+import { SubscribeEntity } from 'src/app/core/models/subscribeEntity';
+import { ArticleService } from '../../services/articlesService';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-themes',
@@ -8,15 +13,49 @@ import { ThemeService } from '../../services/themeService';
   styleUrls: ['./themes.component.scss']
 })
 export class ThemesComponent implements OnInit {
-
+  private apiUrl = environment.baseUrl;
   themes: themeEntity[] = [];
+  // subscribes: SubscribeEntity[] = [];
+  subscribedSet$ = new BehaviorSubject<Set<number>>(new Set<number>());
 
-  constructor(private themeService: ThemeService) { }
+
+  constructor(private themeService: ThemeService,
+              private articleService: ArticleService,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
     this.themeService.fetch().subscribe((response: themeEntity[]) => {
       this.themes = response;
     });
+
+    this.articleService.getAllSubscribes().subscribe((response: SubscribeEntity[]) => {
+      const set = new Set(response.map(subscription => subscription.themeId));
+      this.subscribedSet$.next(set);
+    });
   }
+
+  doesIdexist(id: number): boolean {
+    const set = this.subscribedSet$.getValue();
+    return set.has(id);
+  }
+
+  onClickSubscribe(id: number):void {
+    this.http.post<void>(`${this.apiUrl}subscribe/` + id, null).subscribe(() => {
+      const set = this.subscribedSet$.getValue();
+      set.add(id);
+      this.subscribedSet$.next(set);
+    });
+    
+  }
+
+  onClickUnSubscribe(id: number):void {
+    this.http.post<void>(`${this.apiUrl}unsubscribe/` + id, null).subscribe(() => {
+      const set = this.subscribedSet$.getValue();
+      set.delete(id);
+      this.subscribedSet$.next(set);
+    });
+    
+  }
+
 
 }
