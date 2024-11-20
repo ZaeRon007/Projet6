bfe2d75cafe184244da23cf31bade0c57153089b
ZaeRon007 <julien.pelissier8@orange.fr>
2024-11-20 09:35:57 +0100

    update for me component : view of subscribes gesture

---
+++ front/src/app/core/models/dto/displayTheme.ts
@@ -0,0 +1,5 @@
+export class DisplayThemes{
+    id!: number;
+    title!: string;
+    content!: string;
+}
--- front/src/app/mddapi/components/me/me.component.ts
+++ front/src/app/mddapi/components/me/me.component.ts
@@ -1,9 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { userEntity } from 'src/app/core/models/userEntity';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { environment } from 'src/environments/environment.prod';
+import { ArticleService } from '../../services/articlesService';
+import { SubscribeEntity } from 'src/app/core/models/subscribeEntity';
+import { BehaviorSubject, forkJoin, map, switchMap } from 'rxjs';
+import { ThemeService } from '../../services/themeService';
+import { DisplayThemes } from 'src/app/core/models/dto/displayTheme';
 
 @Component({
   selector: 'app-me',
@@ -12,16 +16,38 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class MeComponent implements OnInit {
   private apiUrl = environment.baseUrl;
+  displayThemes: DisplayThemes[] = [new DisplayThemes()];
   
   user: userEntity = {name: "name", email: "email"};
   
   constructor(private http: HttpClient,
-              private authService: AuthService){
+              private authService: AuthService,
+              private articleService: ArticleService,
+              private themeService: ThemeService){
   }
 
   ngOnInit(): void {
     this.http.get<userEntity>(`${this.apiUrl}auth/me`).subscribe((response : userEntity) => {
       this.user = response;
+    });
+
+    this.articleService.getAllSubscribes().pipe(
+      switchMap((response: SubscribeEntity[]) => {
+        const displayThemes$ = response.map(theme => 
+          this.themeService.getThemeById(theme.themeId).pipe(
+            map(res => {
+              const displayThemes: DisplayThemes = new DisplayThemes();
+              displayThemes.id = res.id;
+              displayThemes.title = res.name;
+              displayThemes.content = res.content;
+              return displayThemes;
+            })
+        )
+      );
+      return forkJoin(displayThemes$);
+      })
+    ).subscribe((response: DisplayThemes[]) => {
+      this.displayThemes = response;
     });
   }
 
@@ -44,6 +70,11 @@ export class MeComponent implements OnInit {
   //     console.log("nothing changed!!");
   //     return false;
   //   }
+  }
+
+  onClickUnSubscribe(id: number):void {
+    this.http.post<void>(`${this.apiUrl}unsubscribe/` + id, null).subscribe();
+    
   }
 
 }
