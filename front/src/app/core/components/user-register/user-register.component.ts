a42d386b7e9fce6456108ee6d3c45d07a8e4ac8a
ZaeRon007 <julien.pelissier8@orange.fr>
2024-11-25 14:54:32 +0100

    update for password gesture

--- front/src/app/core/components/user-register/user-register.component.ts
+++ front/src/app/core/components/user-register/user-register.component.ts
@@ -12,14 +12,18 @@ import { AuthService } from '../../services/auth.service';
 export class UserRegisterComponent implements OnDestroy{
   user: AuthRequest = {email: "", name: "", password: ""};
   registerSubscription: Subscription = new Subscription();
-
+  private passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
 
   constructor(private router: Router,
               private authService: AuthService) { 
   }
 
   isFormValid(): boolean {
-    return !!this.user.name.trim() && !!this.user.email.trim() && !!this.user.password.trim();
+    return !!this.user.name.trim() && !!this.user.email.trim() && !!this.isPasswordValid();
+  }
+
+  isPasswordValid(): boolean {
+    return this.passwordRegex.test(this.user.password);
   }
 
   onSubmit():void {
